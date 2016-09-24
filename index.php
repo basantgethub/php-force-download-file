4f57eb2d6b0de07707c1ff10677ad88ece1b5ff7
Basant kumar <basantgithub@gmail.com>
2016-09-24 06:13:26 -0400

    Update and rename LICENSE.md to index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>How to Force Download File With PHP</title>
+	</head>
+	<body>
+		<h1>
+			Demo Force Download File With PHP
+		</h1>
+		<hr />
+		<br />
+		<h3>ZIP File Download</h3>
+		<a href="download.php?file=php-force-download-file.rar" target="_new">
+			<button class="style_btn">
+				Dowload File
+			</button>
+		</a>
+		<br />
+		<br />
+		<h3>Image File Download</h3>
+		<a href="download.php?file=basant.jpg" target="_new">
+			<button class="style_btn">
+				Dowload File
+			</button>
+		</a>
+	</body>
+</html>
